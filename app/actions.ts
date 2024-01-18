ff8b891f6d833649fe7605d13459cb51482c0fb6
Steven Tey <stevensteel97@gmail.com>
2024-01-18 15:42:29 -0800

    Added dublet

---
+++ app/actions.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import Dub from "dub";
+
+const dub = new Dub({
+  token: process.env.DUB_API_KEY,
+  projectSlug: "dub",
+});
+
+export async function shorten(_prevState: any, formData: FormData) {
+  const url = formData.get("url");
+  if (!url || typeof url !== "string") {
+    return {
+      shortLink: "Invalid URL",
+    };
+  }
+  const response = await dub.links.create({ domain: "dub.sh", url });
+
+  return {
+    shortLink: `https://${response.domain}/${response.key}`,
+  };
+}
