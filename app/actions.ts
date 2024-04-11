192f085be0968873e3e8d865f76fc000cd511a40
Steven Tey <stevensteel97@gmail.com>
2024-04-11 20:34:54 +0300

    upgrade version

--- app/actions.ts
+++ app/actions.ts
@@ -1,9 +1,9 @@
 "use server";
 
-import Dub from "dub";
+import { Dub } from "dub";
 
 const dub = new Dub({
-  projectSlug: "acme",
+  workspaceId: "ws_clrei1gld0002vs9mzn93p8ik",
 });
 
 export async function shorten(_prevState: any, formData: FormData) {
@@ -14,8 +14,7 @@ export async function shorten(_prevState: any, formData: FormData) {
     };
   }
   const { shortLink } = await dub.links.create({
-    domain: "dub.sh", // optional param – if not set the primary domain will be used
-    key: "my-key", // optional – if not set, a random 7-char key will be generated
+    domain: "dub.sh", // optional param – if not set the primary domain will be used
     url, // required – the URL to shorten
     prefix: "/c/", // optional – if set, generated keys will be in the /c/:key format
     tagIds: ["clsy3dq5y000ukc4qw9wg96yj"], // optional – tags to associate with the link
