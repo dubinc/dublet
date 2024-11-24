a9112d568cf08229a9fe627e99e560219f45dd25
Steven Tey <stevensteel97@gmail.com>
2024-11-24 15:36:37 -0800

    update dub version

--- app/actions.ts
+++ app/actions.ts
@@ -12,7 +12,7 @@ export async function shorten(_prevState: any, formData: FormData) {
   const { shortLink } = await dub.links.create({
     domain: "dub.sh", // optional param – if not set the primary domain will be used
     url, // required – the URL to shorten
-    tagNames: ["random tag"], // optional param – the tags to associate with the link (can also be tagIds)
+    tagIds: ["tag_txxHuppW1IjzZueUtFbf8cNt"], // optional param – the tags to associate with the link
     // externalId: "ext_x12345677", // optional param – the unique ID of the link in your database
   });
 
